a170a1bd828f3c5aeecf064759dc128ec4a89f98
Marcelo Bini <marcelobini@gmail.com>
2020-04-01 17:43:11 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,10 @@
 # Write your methods here
 def loop_message_five_times
   count = 0
-  while count < 5
+  while count < 5 do
   puts "Hello World"
   count += 1
+ end
 end
 
 def loop_message_n_times
